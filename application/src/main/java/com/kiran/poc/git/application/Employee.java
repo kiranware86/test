84983f2fe76e8ca9d2a2dad2c67798269eca8f49
kiranware13 <kiranware13@gmail.com>
2020-09-15 01:20:29 +0530

    Getters and setters added into Employee.java

--- application/src/main/java/com/kiran/poc/git/application/Employee.java
+++ application/src/main/java/com/kiran/poc/git/application/Employee.java
@@ -8,6 +8,54 @@ public class Employee {
 	private String empEmailId;
 	private String qualification;
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmpDesignation() {
+		return empDesignation;
+	}
+
+	public void setEmpDesignation(String empDesignation) {
+		this.empDesignation = empDesignation;
+	}
+
+	public String getEmpEmailId() {
+		return empEmailId;
+	}
+
+	public void setEmpEmailId(String empEmailId) {
+		this.empEmailId = empEmailId;
+	}
+
+	public String getQualification() {
+		return qualification;
+	}
+
+	public void setQualification(String qualification) {
+		this.qualification = qualification;
+	}
+
 	public Employee() {
 		// No arg constructor
 	}
